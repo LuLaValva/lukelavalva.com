b7bdf6628aad9203cabef80a77d6fc8e9d533b63
LuLaValva <lulavalva@gmail.com>
2021-08-01 16:27:47 -0400

    Basic website framework

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-function App() {
+import NavigationHandler from "./NavigationHandler";
+import Home from "./Pages/Home";
+import SubPage from "./Pages/SubPage";
+import AlbumRatingPage from "./Pages/AlbumRatingPage";
+
+let App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <NavigationHandler>
+        <Home />
+        <SubPage>
+          <Switch>
+            <Route path="/albums" component={AlbumRatingPage} />
+          </Switch>
+        </SubPage>
+      </NavigationHandler>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
