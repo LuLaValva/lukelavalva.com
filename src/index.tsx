9baf34c854cdde7c5b2976f8c24786c8fedd119c
LuLaValva <lulavalva@gmail.com>
2022-01-13 23:35:12 -0500

    vh override for mobile users

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,16 @@ import Amplify from "aws-amplify";
 import awsExports from "./aws-exports";
 Amplify.configure(awsExports);
 
+const resetCssVh = () => {
+  document.documentElement.style.setProperty(
+    "--vh",
+    `${window.innerHeight / 100}px`
+  );
+};
+
+resetCssVh();
+window.addEventListener("resize", resetCssVh);
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
