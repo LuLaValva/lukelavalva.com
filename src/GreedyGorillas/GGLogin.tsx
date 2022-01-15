87b2eae80929695de1ae5314e3c32fcba93ac094
LuLaValva <lulavalva@gmail.com>
2022-01-15 01:34:09 -0500

    Added game description and modified UI for mobile

--- src/GreedyGorillas/GGLogin.tsx
+++ src/GreedyGorillas/GGLogin.tsx
@@ -21,17 +21,49 @@ const GGLogin = (props: Props) => {
   };
 
   return (
-    <div className={`${styles.loginContainer}`}>
-      <div>
-        Room Key: <input name="room" onChange={handleRoomChange} />
+    <>
+      <div className={styles.gameDescriptionBox}>
+        <h1>Greedy Gorillas</h1>
+        <h3>A game of lies and deceit</h3>
+        <p>
+          This game is designed to be played by a group of 3-23 people, each of
+          whom are hiding their devices from one another.
+        </p>
+        <p>
+          When the game begins, each player will be assigned a role from a
+          predefined list. This role is to be kept secret, as no player is made
+          aware of anyone else's role right away.
+        </p>
+        <p>
+          Gorillas will take turns in a round-robin fashion. On their turn, a
+          gorilla may perform the action associated with their role or they can
+          pretend to be any other role.
+        </p>
+        <p>
+          It is important for you to know what each role does, but I'm not gonna
+          tell you here. If you really care to find out send me an email or
+          something.
+        </p>
+        <p>
+          To begin the game, get all your friends together in a virtual or
+          physical location where you can all hear each other talking and decide
+          on a name for your room. Everyone in your party must have the same
+          room key. It is recommended that multiple people don't use the same
+          name because that could quickly become confusing, but it is allowed.
+        </p>
       </div>
-      <div>
-        Player Name: <input name="username" onChange={handleUsernameChange} />
+      <div className={`${styles.loginContainer}`}>
+        <div>
+          Room Key: <input name="room" onChange={handleRoomChange} />
+        </div>
+        <div>
+          Player Name: <input name="username" onChange={handleUsernameChange} />
+        </div>
+        <button onClick={joinRoom} className={styles.joinRoomButton}>
+          Join Room
+        </button>
       </div>
-      <button onClick={joinRoom} className={styles.joinRoomButton}>
-        Join Room
-      </button>
-    </div>
+    </>
   );
 };
 
